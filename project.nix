873d10273554c1315b44a6ec1ba5e34c5e4a1b63
Răzvan Flavius Panda <razvan.panda@gmail.com>
2020-06-18 22:35:03 +0100

    Added package Text

--- project.nix
+++ project.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, stdenv }:
+{ mkDerivation, base, stdenv, text }:
 mkDerivation {
   pname = "Clash-of-Code";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base ];
+  executableHaskellDepends = [ base text ];
   license = "unknown";
   hydraPlatforms = stdenv.lib.platforms.none;
 }
